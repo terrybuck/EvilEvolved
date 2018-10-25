b940960461fdc06fed567d8e26bd0a1aceefd910
Terry Buck <terry.j.buck@gmail.com>
2018-10-25 18:43:09 -0400

    Completed the basic intro to using Win2D in visual studio 2017 c#

--- EvilEvolved/EvilEvolved/MainPage.xaml.cs
+++ EvilEvolved/EvilEvolved/MainPage.xaml.cs
@@ -6,7 +6,9 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -17,14 +19,17 @@ using Windows.UI.Xaml.Navigation;
 
 namespace EvilEvolved
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        void CanvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.DrawEllipse(155, 115, 80, 30, Colors.Black, 3);
+            args.DrawingSession.DrawText("Hello, world!", 100, 100, Colors.Yellow);
         }
     }
 }
