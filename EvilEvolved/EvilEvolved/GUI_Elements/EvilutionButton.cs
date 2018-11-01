79601e390582266541ea018edbeb35cb73574f54
Terry Buck <terry.j.buck@gmail.com>
2018-11-01 12:16:15 -0400

    mouse hover detection added to button

--- EvilEvolved/EvilEvolved/GUI_Elements/EvilutionButton.cs
+++ EvilEvolved/EvilEvolved/GUI_Elements/EvilutionButton.cs
@@ -16,18 +16,46 @@ namespace EvilutionClass
         {
             this.Text = text;
             this.BorderColor = Colors.White;
+            this.HighlightBorderColor = Colors.Lime;
         }
 
         public override void Update(TimeSpan dt, GenericInput input)
         {
+            if (input is MouseGenericInput)
+            {
+                MouseGenericInput mgi = (MouseGenericInput)input;
+                switch(mgi.MouseInputType)
+                {
+                    case MouseGenericInputType.MouseMove:
+                        if (mgi.X >this.Location.X && mgi.X < this.Location.X+this.Size.Width &&
+                            mgi.Y > this.Location.Y && mgi.Y < this.Location.Y+this.Size.Height)
+                        {
+                            IsHover = true;
+                        }
+                        else
+                        {
+                            IsHover = false;
+                        }
+                        break;
+                }
+            }
            // base.Update(dt, input);
         }
 
         public override void Draw(CanvasDrawingSession cds)
         {
+            //test rectangle
             Windows.Foundation.Rect rect = new Windows.Foundation.Rect(Location.X, Location.Y, Size.Width, Size.Height);
 
-            cds.DrawRectangle(rect, BorderColor);
+            //draw the border
+            if (IsHover)
+            {
+                cds.DrawRectangle(rect, HighlightBorderColor);
+            }
+            else
+            {
+                cds.DrawRectangle(rect, BorderColor);
+            }
 
             Microsoft.Graphics.Canvas.Text.CanvasTextFormat ctf = new Microsoft.Graphics.Canvas.Text.CanvasTextFormat();
             ctf.VerticalAlignment = Microsoft.Graphics.Canvas.Text.CanvasVerticalAlignment.Center;
@@ -37,6 +65,8 @@ namespace EvilutionClass
         }
 
         public Color BorderColor { get; set; }
+        public Color HighlightBorderColor { get; set; }
+        private bool IsHover { get; set; }
 
         public string Text { get; set; }
     }
